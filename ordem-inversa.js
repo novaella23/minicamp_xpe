4b7bb2b31f5508b09a0c97a7e7250eb6fb6289e3
Elaine D'Adderio <dadderio79@outlook.com>
2023-01-27 20:16:29 -0300

    atualiza

--- ordem-inversa.js
+++ ordem-inversa.js
@@ -1,12 +1,29 @@
 //Criar uma função que recebe uma palavra como parâmetro e retorna esta palavra invertida
 
-function ordemInversa(ROMA){
-    var palavraInvertida = "";
-    for (var i = ROMA.length - 1; i >=0 ; i--){
-        palavraInvertida = palavraInvertida + ROMA[i];
+// function ordemInversa(ROMA){
+//     var palavraInvertida = "";
+//     for (var i = ROMA.length-1; i >=0 ; i--){
+//         palavraInvertida = palavraInvertida + ROMA[i];
        
-    }
-    return palavraInvertida;
-}
+//     }
+//    return palavraInvertida;
+// }
 
-console.log(ordemInversa("AMOR"));
+// console.log(ordemInversa("AMOR"));
+
+
+var lista = [2, 5, 7, 9, 12, 18, 20];
+
+function encontraNumero (numero){
+    var existe = false;
+    
+    for (var i = 0; i < lista.length; i++){
+        if (lista[i] === numero){
+            existe = true;
+            
+            //ao encontrar o número na lista, ele para de verificar no restante da lista.
+        }
+        console.log(i, true) ;
+    }
+ console.log(encontraNumero(18));
+}
