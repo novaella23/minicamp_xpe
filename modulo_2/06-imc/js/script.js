08b8ce9178d2b0986a11466f8b745bbdd3a2d133
novaella23 <dadderio79@outlook.com>
2023-02-04 23:14:28 -0300

    23h13 04-02-23 atualiza

--- modulo_2/06-imc/js/script.js
+++ modulo_2/06-imc/js/script.js
@@ -1,1 +1,43 @@
-console.log('Teste no console do navegador.');
+function start(){
+    var buttonCalculateIMC = document.querySelector('#button-calculate');
+    buttonCalculateIMC.addEventListener('click', handleButtonClick);
+    //          escutador de eventos tipo de evento (quando clicar, executar a função  )               
+    // console.log(buttonCalculateIMC);
+    //buttonCalculateIMC.textContent = 'modifiquei';  ==> para testar o botão
+
+    var inputWeight = document.querySelector('#input-weight');
+    var inputHeight = document.querySelector('#input-height');
+
+    inputWeight.addEventListener('input', handleButtonClick);
+    inputHeight.addEventListener('input', handleButtonClick);
+
+
+    handleButtonClick();//já no start começa calculando
+
+}
+
+function calculateIMC(weight, height){
+    return weight/(height*height);
+}
+
+function handleButtonClick(){
+    var inputWeight = document.querySelector('#input-weight');
+    var inputHeight = document.querySelector('#input-height');
+    // console.log(inputWeight);
+    // console.log(inputHeight);
+    var imcResultado = document.querySelector('#resultado');
+    
+    var weight = Number(inputWeight.value);//atribuiu a weight um valorxcampo
+    var height = Number(inputHeight.value);
+    // console.log(weight);
+    // console.log(height);
+
+    var imc = calculateIMC(weight, height);
+    // console.log(imc)
+
+    var imcFormatado = imc.toFixed(2).replace('.',',');
+    // imcResultado.textContent = imc;
+    imcResultado.textContent = imcFormatado;
+}
+start();
+
